175e5f9bf1e16d4a7cb94c552790ccdd5011fb50
carina9231 <carina9872@gmail.com>
2021-09-24 23:42:29 +0900

    app.py 게시물 페이지 작성 내용 db 넣기 추가 db 이름: marumaru로 변경

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
-db = client.dogMeet
+db = client.marumaru
 
 # 메인페이지 불러오기
 @app.route('/')
@@ -20,6 +20,32 @@ def show_posts():
 def show_profile():
     profiles = list(db.profile.find({}, {'_id': False}))
     return jsonify({'all_profile': profiles})
+
+
+
+# 게시물 작성페이지 불러오기
+@app.route('/posts/create')
+def show_posts_upload():
+    return render_template('post upload.html')
+
+
+@app.route('/posts/create', methods=['POST'])
+def post_upload():
+    author_receive = request.form['author_give']
+    title_receive = request.form['title_give']
+    address_receive = request.form['address_give']
+    contents_receive = request.form['contents_give']
+
+    doc = {
+        'author': author_receive,
+        'title': title_receive,
+        'contents': contents_receive,
+        'address': address_receive
+    }
+
+    db.posts_create.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
