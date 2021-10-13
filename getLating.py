4ad77cfb0460c4d622627332cbd21e5b6f939591
songyeon <https://github.com/songyeon92>
2021-10-14 06:04:50 +0900

    ADD-사진 폴더 추가

--- getLating.py
+++ getLating.py
@@ -1,9 +1,11 @@
 import requests
 import json
+
+
 def getLatLng(addr):
-    url = 'https://dapi.kakao.com/v2/local/search/address.json?query='+addr
+    url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + addr
     headers = {"Authorization": "KakaoAK 894cfd738b31d10baba806317025d155"}
-    result = json.loads(str(requests.get(url,headers=headers).text))
+    result = json.loads(str(requests.get(url, headers=headers).text))
     match_first = result['documents'][0]['address']
 
-    return float(match_first['y']),float(match_first['x'])
+    return float(match_first['y']), float(match_first['x'])
